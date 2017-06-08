c0ee5fc8daf7f56ba318edd30feeaaa9ca815920
Eryk Mariankowski <erykmariankowski@MacBook-Air-Eryk.local>
2017-06-08 21:39:02 +0200

    Initial Commit

---
+++ fitbook-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitbook-ios
+//
+//  Created by Eryk Mariankowski on 08.06.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
