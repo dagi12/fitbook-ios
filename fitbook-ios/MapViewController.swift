24f16b7fb8d438fa1ac2616e57562221c530022b
Eryk Mariankowski <erykmariankowski@MacBook-Air-Eryk.local>
2017-06-09 13:16:28 +0200

    menu nawigacji

---
+++ fitbook-ios/MapViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MapViewController.swift
+//  fitbook-ios
+//
+//  Created by Eryk Mariankowski on 09.06.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+struct FacebookButtonCoords {
+    static let marginBottom = CGFloat(60)
+    static let width = CGFloat(250)
+    static let height = CGFloat(40)
+}
+
+class MapViewController: UIViewController {
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        let frameHeight = UIScreen.main.bounds.height
+        let buttonHeight = loginButton.frame.height
+        let yCoord = frameHeight - buttonHeight - FacebookButtonCoords.marginBottom
+        var loginButtonFrame = loginButton.frame
+        loginButtonFrame.size.width = FacebookButtonCoords.width
+        loginButtonFrame.size.height = FacebookButtonCoords.height
+        loginButton.frame = loginButtonFrame
+        loginButton.center = CGPoint(x: view.center.x, y: yCoord)
+        view.addSubview(loginButton)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+
+}
--- fitbook-ios/ViewController.swift
+++ fitbook-ios/ViewController.swift
@@ -9,33 +9,16 @@
 import UIKit
 import FacebookLogin
 
-struct FacebookButtonCoords {
-    static let marginBottom = CGFloat(20)
-    static let width = CGFloat(250)
-    static let height = CGFloat(40)
-}
-
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
-        let frameHeight = UIScreen.main.bounds.height
-        let buttonHeight = loginButton.frame.height
-        let yCoord = frameHeight - buttonHeight - FacebookButtonCoords.marginBottom
-        var loginButtonFrame = loginButton.frame
-        loginButtonFrame.size.width = FacebookButtonCoords.width
-        loginButtonFrame.size.height = FacebookButtonCoords.height
-        loginButton.frame = loginButtonFrame
-        loginButton.center = CGPoint(x: view.center.x, y: yCoord)
-        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
