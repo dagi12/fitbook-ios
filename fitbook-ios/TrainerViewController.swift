c49c2c68a0972b0e22b0075a134bfe42503fe197
Eryk Mariankowski <erykmariankowski@MacBook-Air-Eryk.local>
2017-06-09 13:40:53 +0200

    ekran trenera

---
+++ fitbook-ios/TrainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrainerViewController.swift
+//  fitbook-ios
+//
+//  Created by Eryk Mariankowski on 09.06.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import UIKit
+
+class TrainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
